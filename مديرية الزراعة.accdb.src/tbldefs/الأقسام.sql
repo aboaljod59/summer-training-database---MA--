0549ad36b854a38ae8e2345f073d0788d9fb1762
aboaljod59 <78337911+aboaljod59@users.noreply.github.com>
2022-09-11 13:02:30 +0300

    Add files via upload

---
+++ مديرية الزراعة.accdb.src/tbldefs/الأقسام.sql
@@ -0,0 +1,4 @@
+﻿CREATE TABLE [الأقسام] (
+  [الرمز الوظيفي للقسم] LONG  CONSTRAINT [PrimaryKey] PRIMARY KEY UNIQUE NOT NULL,
+  [القسم] VARCHAR (255)
+)
---
+++ مديرية الزراعة.accdb.src/tbldefs/الموظف.sql
@@ -0,0 +1,4 @@
+﻿CREATE TABLE [الموظف] (
+  [الرمز الوظيفي] LONG  CONSTRAINT [PrimaryKey] PRIMARY KEY UNIQUE NOT NULL,
+  [اسم الموظف] VARCHAR (255)
+)
